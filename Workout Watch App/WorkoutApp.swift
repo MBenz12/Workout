5852290eb4234356137ad0e2315c17f8674d180e
mbenz12 <mbenz10120112@gmail.com>
2023-10-26 07:34:30 -0500

    created watch os app

---
+++ Workout Watch App/WorkoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutApp.swift
+//  Workout Watch App
+//
+//  Created by admin on 10/26/23.
+//
+
+import SwiftUI
+
+@main
+struct Workout_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
